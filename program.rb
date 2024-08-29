41776903f38453db8674e80d4f7f43d3bdb01374
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2024-08-29 10:25:22 -0700

    program functions incorrectly

--- program.rb
+++ program.rb
@@ -3,6 +3,22 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 def substrings(target_substring, source_dictionary)
 
+  #go through each string in the array
+  #if every letter in the source_dictionary string 
+  #is contained within the target_substring
+  #push string to hash and return the hash with a count
+  
+  final_hash = []
+  
+  source_dictionary.each do |word|
+    if target_substring.include?(word)
+      final_array.push(word)
+    end
+  end
+
+  return final_array.each_with_object(Hash.new(0)) {|word, acc| acc[word] += 1}
+end
+
 
 
 
@@ -10,3 +26,7 @@ def substrings(target_substring, source_dictionary)
 
 substrings("below", dictionary)
 #=> { "below" => 1, "low" => 1 }
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+#=> { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+
