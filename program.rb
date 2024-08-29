8bfe1c3e6ca8feefd95ab2385dabe597286b2d25
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2024-08-29 09:44:14 -0700

    skeleton program

---
+++ program.rb
@@ -0,0 +1,12 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+
+def substrings(target_substring, source_dictionary)
+
+
+
+
+
+
+substrings("below", dictionary)
+#=> { "below" => 1, "low" => 1 }
